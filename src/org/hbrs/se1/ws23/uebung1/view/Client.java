d48283fd2887cd54174a6afc4a925cf6ddfb70e5
Xin667 <abcwdx@gmail.com>
2023-10-15 20:07:43 +0200

    kein "new" in Client, die Methode gibt das Ergebnis von Übersetzen zurück

--- src/org/hbrs/se1/ws23/uebung1/view/Client.java
+++ src/org/hbrs/se1/ws23/uebung1/view/Client.java
@@ -1,26 +1,23 @@
 package org.hbrs.se1.ws23.uebung1.view;
 
-public class Client {
+import org.hbrs.se1.ws23.uebung1.control.GermanTranslator;
+import org.hbrs.se1.ws23.uebung1.control.TranslatorFactory;
 
-		/*
-		 * Methode zur Ausgabe einer Zahl auf der Console
-		 * (auch bezeichnet als CLI, Terminal)
-		 *
-		 */
-		 void display( int aNumber ){
-			// In dieser Methode soll die Methode translateNumber
-			// mit dem übergegebenen Wert der Variable aNumber
-			// aufgerufen werden.
-			//
-			// Strenge Implementierung gegen das Interface Translator gewuenscht!
+import java.util.ArrayList;
 
-			System.out.println("Das Ergebnis der Berechnung: " +
-					"[das Ergebnis an dieser Stelle]"  );
-
-		 }
+public class Client extends GermanTranslator {
+	/*
+	 * Methode zur Ausgabe einer Zahl auf der Console
+	 * (auch bezeichnet als CLI, Terminal)
+	 *
+	 */
+	void display( int zahl ){
+		// In dieser Methode soll die Methode translateNumber
+		// mit dem übergegebenen Wert der Variable aNumber
+		// aufgerufen werden.
+		//
+		// Strenge Implementierung gegen das Interface Translator gewuenscht!
+		String result = TranslatorFactory.creatGermanTranslator().translateNumber(zahl);
+		System.out.println("Das Ergebnis der Berechnung: " + zahl + "ist " + result);
+	}
 }
-
-
-
-
-
